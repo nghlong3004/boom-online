4d1be0e2823be7f07e33da192a2be283bb016c2b
nghlong3004 <nghlong3004@gmail.com>
2025-12-08 01:21:29 +0700

    docs: add low level design

--- src/main/resources/db/migration/V1__Init_database.sql
+++ src/main/resources/db/migration/V1__Init_database.sql
@@ -1,7 +1,7 @@
 CREATE TABLE bomber(
     id BIGSERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
     full_name VARCHAR(30) NOT NULL,
     birthday VARCHAR(30) NOT NULL,
     gender VARCHAR(1) NOT NULL ,
